03f5d7dd47322085cbd4f1cbd52f1e69fc51bec1
Renato Lopes <renato.lopes1984@gmail.com>
2024-09-03 22:36:40 -0300

    Condição simples

---
+++ src/estruturas/condicionais/nova/aula/CaixaEletronico.java
@@ -0,0 +1,16 @@
+package estruturas.condicionais.nova.aula;
+
+public class CaixaEletronico {
+
+	public static void main(String[] args) {
+		double saldo = 25.0;
+		double valorSolicitado = 26.0;
+		
+		if(valorSolicitado < saldo)
+			saldo = saldo - valorSolicitado;
+		
+		System.out.println(saldo);
+
+	}
+
+}
