597bb2600f1cedc0bafa163a12651e8e9f91e7e0
Renato Lopes <renato.lopes1984@gmail.com>
2024-09-11 00:28:36 -0300

    Exceções

--- src/estruturas/condicionais/nova/aula/AboutMe.java
+++ src/estruturas/condicionais/nova/aula/AboutMe.java
@@ -26,7 +26,7 @@ public class AboutMe {
 		System.out.println("Digite sua altura");
 		double altura =  scanner.nextDouble();
 		
-		System.out.println("Ola, me chamo " + nome.toUpperCase() + " " + sobrenome);
+		System.out.println("Ola, me chamo " + nome + " " + sobrenome);
 		System.out.println("Tenho " + idade + " anos ");
 		System.out.println("Minha altura é " + altura + "cm ");
 
